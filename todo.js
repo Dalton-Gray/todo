bab82b7c6ebb113a502bd0a5a4a41b6853464b2e
Dalton Gray <daltonagray@gmail.com>
2020-01-19 21:09:47 +0000

    comments

--- todo.js
+++ todo.js
@@ -3,6 +3,7 @@ let id = 0;
 function addItem(){
 	id++;
 
+	// Takes text in text input and turns it into a new li 
 	let item = document.getElementById("todoInput").value;
 	let text = document.createTextNode(item);
 	let newItem = document.createElement("li");
@@ -18,6 +19,7 @@ function addItem(){
 	btn.classList.add("btn-danger");
 	document.getElementById("item" + id).appendChild(btn);
 
+	// Function that removes the item when close button clicked
 	btn.onclick = function() {
 		var div = this.parentElement;
 		div.style.display = "none";
